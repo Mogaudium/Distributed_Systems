8a4335e83410dd7584763c826cca3559bd2d0679
Mogaudium <121038646+Mogaudium@users.noreply.github.com>
2023-11-14 18:57:18 +0200

    Simple staff

--- Client.py
+++ Client.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 import requests
+import pygame
+from io import BytesIO
 
 def login():
     # Login to the server
-    response = requests.post('http://localhost:5000/login', data={"username": "user", "password": "pass"})
+    response = requests.post('http://localhost:5000/login', data={"username": "user1", "password": "password1"})
     print(response.json())
 
 def get_file_list():
@@ -11,13 +13,37 @@ def get_file_list():
     response = requests.get('http://localhost:5000/files')
     print(response.json())
 
+def download_and_play(file_name):
+    response = requests.get(f'http://localhost:5000/file/{file_name}')
+    if response.status_code == 200:
+        audio_data = BytesIO(response.content)
+        pygame.mixer.init()
+        pygame.mixer.music.load(audio_data)
+        pygame.mixer.music.play()
+    else:
+        print("Failed to download file")
+
 root = tk.Tk()
 root.title("Audio Client")
+
+# Login Fields
+username_label = tk.Label(root, text="Username")
+username_label.pack()
+username_entry = tk.Entry(root)
+username_entry.pack()
+
+password_label = tk.Label(root, text="Password")
+password_label.pack()
+password_entry = tk.Entry(root, show="*")
+password_entry.pack()
 
 login_button = tk.Button(root, text="Login", command=login)
 login_button.pack()
 
 list_files_button = tk.Button(root, text="List Files", command=get_file_list)
 list_files_button.pack()
+
+play_button = tk.Button(root, text="Play File", command=lambda: download_and_play('path/to/song1.mp3'))
+play_button.pack()
 
 root.mainloop()
--- Server.py
+++ Server.py
@@ -1,22 +1,36 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 
 app = Flask(__name__)
 
-# Dummy data for example
+# Dummy user data
+users = {
+    "user1": "password1",
+    "user2": "password2"
+}
+
+# Dummy audio file data
 audio_files = {
     "1": "song1.mp3",
-    "2": "song2.mp3",
-    # Add more audio files
+    "2": "song2.mp3"
 }
 
 @app.route('/login', methods=['POST'])
 def login():
-    # Placeholder for login logic
-    return jsonify({"message": "Logged in successfully"})
+    username = request.form['username']
+    password = request.form['password']
+    if username in users and users[username] == password:
+        return jsonify({"message": "Logged in successfully"})
+    else:
+        return jsonify({"message": "Invalid credentials"}), 401
 
 @app.route('/files', methods=['GET'])
 def list_files():
     return jsonify(audio_files)
+
+@app.route('/file/<filename>', methods=['GET'])
+def get_file(filename):
+    # Assuming files are stored in a directory named 'audio_files'
+    return send_from_directory('audio_files', filename)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
