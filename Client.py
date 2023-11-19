b1752ff43b99a4edd0aedd66670e0323a5c33e1e
Mogaudium <121038646+Mogaudium@users.noreply.github.com>
2023-11-19 14:51:30 +0200

    Better GUI

--- Client.py
+++ Client.py
@@ -1,112 +1,125 @@
-import tkinter as tk
-from tkinter import ttk, PhotoImage, messagebox
-import requests
-from io import BytesIO
+import sys
 import pygame
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QLineEdit, QListWidget, QMessageBox
+from PyQt5.QtCore import pyqtSlot
+import requests
+from tempfile import NamedTemporaryFile
 
-# Define colors and styles
-BACKGROUND_COLOR = "#121212"  # Spotify Dark
-FOREGROUND_COLOR = "#FFFFFF"  # White text
-ACCENT_COLOR = "#1DB954"     # Spotify Green
+# Initialize Pygame's mixer with some common frequency and size
+pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
 
-def register():
-    username = username_entry.get()
-    password = password_entry.get()
-    if username and password:  # Ensure that username and password are not empty
-        response = requests.post('http://localhost:5000/register', data={'username': username, 'password': password})
-        print(response.json())  # Print server response
-    else:
-        print("Username or password is empty")  # Debug print
+class LoginWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Login and Register')
+        self.setGeometry(600, 300, 300, 200)
 
-def login():
-    username = username_entry.get()
-    password = password_entry.get()
-    if username and password:  # Ensure that username and password are not empty
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        layout = QVBoxLayout()
+
+        self.username_entry = QLineEdit(self)
+        self.username_entry.setPlaceholderText('Username')
+        layout.addWidget(self.username_entry)
+
+        self.password_entry = QLineEdit(self)
+        self.password_entry.setEchoMode(QLineEdit.Password)
+        self.password_entry.setPlaceholderText('Password')
+        layout.addWidget(self.password_entry)
+
+        login_button = QPushButton('Login', self)
+        login_button.clicked.connect(self.attempt_login)
+        layout.addWidget(login_button)
+
+        register_button = QPushButton('Register', self)
+        register_button.clicked.connect(self.attempt_register)
+        layout.addWidget(register_button)
+
+        self.central_widget.setLayout(layout)
+
+    @pyqtSlot()
+    def attempt_login(self):
+        username = self.username_entry.text()
+        password = self.password_entry.text()
+        # Here, replace with the actual request to your server
         response = requests.post('http://localhost:5000/login', data={'username': username, 'password': password})
-        print(response.json())  # Print server response
-    else:
-        print("Username or password is empty")  # Debug print
+        if response.status_code == 200:
+            self.main_app_window = MainAppWindow()
+            self.main_app_window.show()
+            self.close()
+        else:
+            QMessageBox.warning(self, 'Login failed', 'Incorrect username or password')
 
-def update_file_list():
-    response = requests.get('http://localhost:5000/list-audio')
-    if response.status_code == 200:
-        file_list = response.json()
-        listbox.delete(0, tk.END)  # Clear existing items in the listbox
-        for file_name in file_list:
-            listbox.insert(tk.END, file_name)
-    else:
-        print("Failed to retrieve file list")
+    @pyqtSlot()
+    def attempt_register(self):
+        username = self.username_entry.text()
+        password = self.password_entry.text()
+        # Here, replace with the actual request to your server
+        response = requests.post('http://localhost:5000/register', data={'username': username, 'password': password})
+        if response.status_code == 200:
+            QMessageBox.information(self, 'Registration successful', 'You can now log in with your new credentials.')
+        else:
+            QMessageBox.warning(self, 'Registration failed', 'The registration process has failed.')
 
-def stream_and_play():
-    selected_file = listbox.get(listbox.curselection())
-    url = f'http://localhost:5000/stream/{selected_file}'
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        audio_stream = BytesIO(response.content)
-        pygame.mixer.init()
-        pygame.mixer.music.load(audio_stream)
-        pygame.mixer.music.play()
-    else:
-        print("Failed to stream file")
 
-# Initialize the main window
-root = tk.Tk()
-root.title("Audio Client")
-root.geometry("1024x768")  # Spotify's app is typically full-screen; adjust as needed
-root.configure(bg=BACKGROUND_COLOR)
+class MainAppWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Audio Client')
+        self.setGeometry(100, 100, 800, 600)
 
-# Set the style
-style = ttk.Style(root)
-style.configure('TButton', font=('Helvetica', 12))
-style.configure('TLabel', font=('Helvetica', 12), padding=10)
-style.configure('TFrame', background='light grey')
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        layout = QVBoxLayout()
 
-# Main content area
-content_frame = tk.Frame(root, bg="#d3d3d3")
-content_frame.pack(expand=True, fill=tk.BOTH)
+        self.list_widget = QListWidget(self)
+        layout.addWidget(self.list_widget)
 
-# Left sidebar
-sidebar_frame = tk.Frame(root, bg="#1DB954", width=200, height=768)
-sidebar_frame.pack(side=tk.LEFT, fill=tk.Y)
+        play_button = QPushButton('Play', self)
+        play_button.clicked.connect(self.play_selected_file)
+        layout.addWidget(play_button)
 
-# Playback controls area
-playback_frame = tk.Frame(root, bg="#d3d3d3", height=100)
-playback_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        update_list_button = QPushButton('Update List', self)
+        update_list_button.clicked.connect(self.update_file_list)
+        layout.addWidget(update_list_button)
 
-# Login and Registration Frame
-auth_frame = ttk.Frame(root, style='TFrame')
-auth_frame.pack(pady=10)
+        self.central_widget.setLayout(layout)
 
-username_label = ttk.Label(auth_frame, text="Username", background='light grey')
-username_label.pack(side=tk.LEFT)
-username_entry = ttk.Entry(auth_frame, width=20)
-username_entry.pack(side=tk.LEFT)
+    def play_selected_file(self):
+        selected_item = self.list_widget.currentItem()
+        if selected_item is not None:
+            selected_file = selected_item.text()
+            url = f'http://localhost:5000/stream/{selected_file}'
+            try:
+                # Stream the audio file from the server
+                with requests.get(url, stream=True) as response:
+                    if response.status_code == 200:
+                        # Save the streamed audio to a temporary file
+                        with NamedTemporaryFile(delete=True, suffix='.mp3') as tmp_file:
+                            for chunk in response.iter_content(chunk_size=4096):
+                                tmp_file.write(chunk)
+                            tmp_file.seek(0)  # Rewind the file
+                            # Play the audio file using pygame
+                            pygame.mixer.music.load(tmp_file.name)
+                            pygame.mixer.music.play()
+                    else:
+                        QMessageBox.warning(self, 'Stream Error', 'Failed to stream file')
+            except Exception as e:
+                QMessageBox.warning(self, 'Playback Error', f'An error occurred while trying to play the file: {e}')
 
-password_label = ttk.Label(auth_frame, text="Password", background='light grey')
-password_label.pack(side=tk.LEFT)
-password_entry = ttk.Entry(auth_frame, width=20, show="*")
-password_entry.pack(side=tk.LEFT)
+    @pyqtSlot()
+    def update_file_list(self):
+        response = requests.get('http://localhost:5000/list-audio')
+        if response.status_code == 200:
+            file_list = response.json()
+            self.list_widget.clear()
+            self.list_widget.addItems(file_list)
+        else:
+            QMessageBox.warning(self, 'Error', 'Failed to retrieve file list')
 
-login_button = ttk.Button(auth_frame, text="Login", command=login)
-login_button.pack(side=tk.LEFT, padx=5)
 
-register_button = ttk.Button(auth_frame, text="Register", command=register)
-register_button.pack(side=tk.LEFT)
-
-# File List Frame
-list_frame = ttk.Frame(root, style='TFrame')
-list_frame.pack(pady=20)
-
-listbox_label = ttk.Label(list_frame, text="Available Files", background='light grey')
-listbox_label.pack()
-
-listbox = tk.Listbox(list_frame, width=50, height=15)
-listbox.pack()
-
-update_button = ttk.Button(list_frame, text="Update File List", command=update_file_list)
-update_button.pack(pady=10)
-
-play_button = ttk.Button(sidebar_frame, text="Play Selected File", command=stream_and_play)
-play_button.pack()
-
-root.mainloop()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    login_win = LoginWindow()
+    login_win.show()
+    sys.exit(app.exec_())
